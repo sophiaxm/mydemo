d88f9f8b51d5c61ad453c9c83efcf82bb99526bd
xm <shophia@126.com>
2020-08-01 23:13:51 +0800

    xm_dev2 update

--- spring-demo/src/main/java/com/kgc/SpringDemoApplication.java
+++ spring-demo/src/main/java/com/kgc/SpringDemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+
 @SpringBootApplication
 @PropertySource("classpath:/myconfig/myconfig.properties")
 public class SpringDemoApplication {
